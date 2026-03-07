86159c0bf3a6455dd92c68211064094018101eab
jzplp <jzplp@qq.com>
2026-03-07 20:36:41 +0800

    继续超时

--- 1 copy.cpp
+++ 1 copy.cpp
@@ -1,14 +1,9 @@
 #include <stdio.h>
-#include <string.h>
-#include <set>
-using namespace std;
 
 int n;
 int arr[10000];
-int maxn, sumn;
+int max, sum;
 int num;
-set<int> se;
-
 
 bool judge(int t)
 {
@@ -40,35 +35,19 @@ bool judge(int t)
   return false;
 }
 
-// 拿到可能的木棍长度
-void getNums(int t)
-{
-  if(t == n) return;
-  int i, j, k;
-  if(arr[t] == 0) return getNums(t + 1);
-  // 不处理
-  getNums(t + 1);
-  for(i = t + 1; i < n; ++i) {
-    j = arr[t] + arr[i];
-    if(j > sumn / 2) continue;
-    if(!se.count(j) && !(sumn % j)) se.insert(j);
-    k = arr[i];
-    arr[i] = j;
-    getNums(t + 1);
-    arr[i] = k;
-  }
-}
-
 int compute()
 {
   int i, j, k;
-  for (auto ip = se.begin(); ip != se.end(); ++ip)
+  for (i = max; i < sum / 2 + 1; ++i)
   {
-    num = *ip;
+    // 筛选出能整除的i
+    if (sum % i)
+      continue;
+    num = i;
     if (judge(0))
-      return *ip;
+      return i;
   }
-  return sumn;
+  return sum;
 }
 
 int main()
@@ -76,19 +55,16 @@ int main()
   int i;
   while (scanf("%d", &n) == 1 && n != 0)
   {
-    se.clear();
-    maxn = 0;
-    sumn = 0;
+    max = 0;
+    sum = 0;
     for (i = 0; i < n; ++i)
     {
       scanf("%d", &arr[i]);
-      sumn += arr[i];
-      if (arr[i] > maxn)
-        maxn = arr[i];
+      sum += arr[i];
+      if (arr[i] > max)
+        max = arr[i];
     }
-    if(!(sumn % maxn)) se.insert(maxn);
-    getNums(0);
-    // printf("%d\n", compute());
+    printf("%d\n", compute());
   }
   return 0;
 }
