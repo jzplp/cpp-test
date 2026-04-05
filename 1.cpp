506c0f95a0212d23bf3c9694edb67a56797a8e16
jzplp <jzplp@qq.com>
2026-04-05 21:54:51 +0800

    ac

--- 1.cpp
+++ 1.cpp
@@ -4,6 +4,7 @@
 int arrWall[8][8];
 int Steps[4][2] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
 char StepConvert[4] = {'W', 'N', 'E', 'S'};
+char StepRe[4] = {2, 3, 0, 1};
 int stepArr[100000];
 
 int stepMax;
@@ -13,6 +14,31 @@ bool outXY(int x, int y)
     if (x < 0 || x >= 4 || y < 0 || y >= 6)
         return true;
     return false;
+}
+
+void setWall(int x, int y, int i, bool set)
+{
+    int newX = x + Steps[i][0];
+    int newY = y + Steps[i][1];
+    int newnewX = newX + Steps[i][0];
+    int newnewY = newY + Steps[i][1];
+    int k = 1 << i;
+    int rei = StepRe[i];
+    int rek = 1 << rei;
+    if (set)
+    {
+        arrWall[newX][newY] -= rek;
+        arrWall[newX][newY] += k;
+        arrWall[x][y] -= k;
+        if (!outXY(newnewX, newnewY))
+            arrWall[newnewX][newnewY] += rek;
+        return;
+    }
+    arrWall[newX][newY] += rek;
+    arrWall[newX][newY] -= k;
+    arrWall[x][y] += k;
+    if (!outXY(newnewX, newnewY))
+        arrWall[newnewX][newnewY] -= rek;
 }
 
 bool computed(int x, int y, int stepNum)
@@ -39,18 +65,14 @@ bool computed(int x, int y, int stepNum)
             // 再隔一层也有墙
             if (arrWall[newX][newY] & k)
                 continue;
-            arrWall[newX][newY] += k;
-            arrWall[x][y] -= k;
+            setWall(x, y, i, true);
         }
         stepArr[stepNum] = i;
         if (computed(newX, newY, stepNum + 1))
             return true;
         // 回退墙
         if (hasWall)
-        {
-            arrWall[newX][newY] -= k;
-            arrWall[x][y] += k;
-        }
+            setWall(x, y, i, false);
     }
     return false;
 }
@@ -70,9 +92,7 @@ int main()
         for (i = 0; i < 4; ++i)
         {
             for (j = 0; j < 6; ++j)
-            {
                 scanf("%d", &arrWall[i][j]);
-            }
         }
         for (i = 0;; ++i)
         {
@@ -81,9 +101,7 @@ int main()
                 break;
         }
         for (j = 0; j < i; ++j)
-        {
             putchar(StepConvert[stepArr[j]]);
-        }
         putchar('\n');
     }
     return 0;
