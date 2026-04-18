31051682ad63d8ad923797ad38bd1dcfc67d3d20
jzplp <jzplp@qq.com>
2026-04-18 23:11:36 +0800

    ac

--- 1.cpp
+++ 1.cpp
@@ -1,107 +1,78 @@
 #include <stdio.h>
-#include <string.h>
 
-int arrWall[8][8];
-int Steps[4][2] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
-char StepConvert[4] = {'W', 'N', 'E', 'S'};
-char StepRe[4] = {2, 3, 0, 1};
-int stepArr[100000];
+int arrOrigin[35];
+int len;
+int arr[35];
+int stepArr[70];
+int stepLen;
 
-int stepMax;
-
-bool outXY(int x, int y)
+int findMax(int i)
 {
-    if (x < 0 || x >= 4 || y < 0 || y >= 6)
-        return true;
-    return false;
-}
-
-void setWall(int x, int y, int i, bool set)
-{
-    int newX = x + Steps[i][0];
-    int newY = y + Steps[i][1];
-    int newnewX = newX + Steps[i][0];
-    int newnewY = newY + Steps[i][1];
-    int k = 1 << i;
-    int rei = StepRe[i];
-    int rek = 1 << rei;
-    if (set)
+    int maxi = i;
+    for (int j = i - 1; j >= 0; --j)
     {
-        arrWall[newX][newY] -= rek;
-        arrWall[newX][newY] += k;
-        arrWall[x][y] -= k;
-        if (!outXY(newnewX, newnewY))
-            arrWall[newnewX][newnewY] += rek;
-        return;
+        if (arr[j] > arr[maxi])
+            maxi = j;
     }
-    arrWall[newX][newY] += rek;
-    arrWall[newX][newY] -= k;
-    arrWall[x][y] += k;
-    if (!outXY(newnewX, newnewY))
-        arrWall[newnewX][newnewY] -= rek;
+    return maxi;
 }
 
-bool computed(int x, int y, int stepNum)
+void flip(int i)
 {
-    if (outXY(x, y))
-        return true;
-    if (stepNum >= stepMax)
-        return false;
-    int i, j, k, newX, newY;
-    bool hasWall;
-    for (i = 0; i < 4; ++i)
+    int t;
+    for (int j = 0; j <= i / 2; ++j)
     {
-        k = 1 << i;
-        newX = x + Steps[i][0];
-        newY = y + Steps[i][1];
-        hasWall = false;
-        // 有墙
-        if (arrWall[x][y] & k)
+        t = arr[j];
+        arr[j] = arr[i - j];
+        arr[i - j] = t;
+    }
+}
+
+void loop()
+{
+    int i, maxi;
+    for (i = len - 1; i > 0; --i)
+    {
+        maxi = findMax(i);
+        if (maxi == i)
+            continue;
+        if (maxi != 0)
         {
-            hasWall = true;
-            // 到边缘
-            if (outXY(newX, newY))
-                continue;
-            // 再隔一层也有墙
-            if (arrWall[newX][newY] & k)
-                continue;
-            setWall(x, y, i, true);
+            flip(maxi);
+            stepArr[stepLen++] = maxi;
         }
-        stepArr[stepNum] = i;
-        if (computed(newX, newY, stepNum + 1))
-            return true;
-        // 回退墙
-        if (hasWall)
-            setWall(x, y, i, false);
+        flip(i);
+        stepArr[stepLen++] = i;
     }
-    return false;
 }
 
 int main()
 {
-    int x, y, t;
-    int i, j, k;
-    while (scanf("%d %d", &x, &y) == 2 && x != 0 && y != 0)
+    int i;
+    while (1)
     {
-        t = x;
-        x = y;
-        y = t;
-        x = x - 1;
-        y = y - 1;
-        memset(arrWall, 0, sizeof(arrWall));
-        for (i = 0; i < 4; ++i)
+        len = 0;
+        while (scanf("%d", &arrOrigin[len]) > 0)
         {
-            for (j = 0; j < 6; ++j)
-                scanf("%d", &arrWall[i][j]);
-        }
-        for (i = 0;; ++i)
-        {
-            stepMax = i;
-            if (computed(x, y, 0))
+            arr[len] = arrOrigin[len];
+            ++len;
+            if (getchar() == '\n')
                 break;
         }
-        for (j = 0; j < i; ++j)
-            putchar(StepConvert[stepArr[j]]);
+        if (len == 0)
+            return 0;
+        stepLen = 0;
+        loop();
+        for (i = 0; i < len; ++i)
+        {
+            if (i != 0)
+                putchar(' ');
+            printf("%d", arrOrigin[i]);
+        }
+        putchar('\n');
+        for (i = 0; i < stepLen; ++i)
+            printf("%d ", len - stepArr[i]);
+        putchar('0');
         putchar('\n');
     }
     return 0;
