935da689f14acc94fc893fa9778068578b3da52f
jzplp <jzplp@qq.com>
2026-08-01 00:03:46 +0800

    ac

--- 1.cpp
+++ 1.cpp
@@ -1,107 +1,40 @@
 #include <stdio.h>
-#include <string.h>
-#define MAXN 505
+#include <queue>
+#include <vector>
+#define MAXN 5002
+using namespace std;
 
+int n;
 int arr[MAXN];
-int arrV[MAXN];
-int m, k;
-long long sum, max;
 
-bool getSum(long long v)
+bool cmp(int left, int right)
 {
-  int ki = k;
-  int i, j;
-  long long sumt = 0;
-  for (i = 0; i < m; ++i)
-  {
-    sumt += arr[i];
-    if (sumt > v)
-    {
-      --ki;
-      sumt = arr[i];
-    }
-    if (ki <= 0)
-      return false;
-  }
-  return true;
-}
-
-long long computed(long long beg, long long end)
-{
-  long long mid = (beg + end) / 2;
-  if (mid == beg)
-  {
-    if (getSum(mid))
-      return mid;
-    return end;
-  }
-  if (getSum(mid))
-    end = mid;
-  else
-    beg = mid;
-  return computed(beg, end);
-}
-
-void getPrint(long long v)
-{
-  int i, j, ki = k - 1;
-  long long sumt = 0;
-  for (i = m - 1; i >= 0; --i)
-  {
-    if (i <= ki)
-    {
-      if (sumt + arr[i] > v)
-      {
-        --ki;
-        arrV[i] = ki;
-        sumt = arr[i];
-        continue;
-      }
-      arrV[i] = ki--;
-      sumt = 0;
-      continue;
-    }
-    sumt += arr[i];
-    arrV[i] = ki;
-    if (sumt > v)
-    {
-      --ki;
-      sumt = arr[i];
-      arrV[i] = ki;
-    }
-  }
+  return left > right;
 }
 
 int main()
 {
-  int i, j, N;
-  long long v;
-  scanf("%d", &N);
-  while (N--)
+  int i, j, a1, a2, sum;
+  while (scanf("%d", &n) > 0 && n != 0)
   {
+    priority_queue<int, vector<int>, bool (*)(int, int)> qu(cmp);
+    for (i = 0; i < n; ++i)
+    {
+      scanf("%d", &j);
+      qu.push(j);
+    }
     sum = 0;
-    max = 0;
-    scanf("%d %d", &m, &k);
-    for (i = 0; i < m; ++i)
+    while (qu.size() != 1)
     {
-      scanf("%d", &arr[i]);
-      sum += arr[i];
-      if (arr[i] > max)
-        max = arr[i];
+      a1 = qu.top();
+      qu.pop();
+      a2 = qu.top();
+      qu.pop();
+      a1 += a2;
+      sum += a1;
+      qu.push(a1);
     }
-    v = computed(max, sum);
-    getPrint(v);
-    for (i = 0; i < m; ++i)
-    {
-      printf("%d", arr[i]);
-      if (i + 1 != m)
-      {
-        printf(" ");
-        if (arrV[i + 1] != arrV[i])
-          printf("/ ");
-      }
-    }
-    putchar('\n');
+    printf("%d\n", sum);
   }
   return 0;
 }
