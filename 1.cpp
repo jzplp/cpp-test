b32dbff5619a21b8ded7a3f81200707f84aa7a5a
jzplp <jzplp@qq.com>
2026-08-08 00:58:52 +0800

    ac

--- 1.cpp
+++ 1.cpp
@@ -1,33 +1,57 @@
 #include <stdio.h>
-#include <math.h>
 
-long long func(int k)
-{
-  if (k == 0)
-    return 1;
-  return (long long)(pow(3, k));
-}
+#define MAXN 100005
 
-long long fun(int k, int i)
+int arr1[MAXN];
+int arr2[MAXN];
+int n;
+
+int computed()
 {
-  if (i == 0)
-    return 0;
-  if (k == 0)
-    return 1;
-  int half = 1 << (k - 1);
-  if (i <= half)
-    return 2 * fun(k - 1, i);
-  return 2 * func(k - 1) + fun(k - 1, i - half);
+  int i = 0, j, k;
+  long long sum = 0;
+  bool flag;
+  while (i < n)
+  {
+    sum = 0;
+    for (j = i; j < n + i; ++j)
+    {
+      sum += arr1[j % n];
+      sum -= arr2[j % n];
+      if (sum < 0)
+        break;
+    }
+    if (i == j)
+      ++j;
+    if (j == n + i)
+      return i;
+    if (j < n)
+    {
+      i = j;
+      continue;
+    }
+    return -1;
+  }
+  return -1;
 }
 
 int main()
 {
-  int t, ti, k, a, b;
-  scanf("%d", &t);
-  ti = 0;
-  while (t--)
+  int t = 0, T, i, res;
+  scanf("%d", &T);
+  while (T > t++)
   {
-    scanf("%d %d %d", &k, &a, &b);
-    printf("Case %d: %lld\n", ++ti, fun(k, b) - fun(k, a - 1));
+    scanf("%d", &n);
+    for (i = 0; i < n; ++i)
+      scanf("%d", &arr1[i]);
+    for (i = 0; i < n; ++i)
+      scanf("%d", &arr2[i]);
+    res = computed();
+    printf("Case %d: ", t);
+    if (res == -1)
+      printf("Not possible\n");
+    else
+      printf("Possible from station %d\n", res + 1);
   }
+  return 0;
 }
