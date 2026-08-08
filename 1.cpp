03837cf9851ed0b79f43be63c810f014ab436974
jzplp <jzplp@qq.com>
2026-08-08 22:51:00 +0800

    进展

--- 1.cpp
+++ 1.cpp
@@ -1,57 +1,18 @@
-#include <stdio.h>
-
+#include<stdio.h>
 #define MAXN 100005
+#define MAXM 200005
 
-int arr1[MAXN];
-int arr2[MAXN];
-int n;
+int arrInput[2 * MAXN];
+int n, m;
 
-int computed()
-{
-  int i = 0, j, k;
-  long long sum = 0;
-  bool flag;
-  while (i < n)
-  {
-    sum = 0;
-    for (j = i; j < n + i; ++j)
-    {
-      sum += arr1[j % n];
-      sum -= arr2[j % n];
-      if (sum < 0)
-        break;
+int main() {
+  int d, i;
+  scanf("%d", &d);
+  while(d--) {
+    scanf("%d %d", &n, &m);
+    for(i = 1; i <= 2*m; ++i) {
+      scanf("%d", &arrInput[i]);
     }
-    if (i == j)
-      ++j;
-    if (j == n + i)
-      return i;
-    if (j < n)
-    {
-      i = j;
-      continue;
-    }
-    return -1;
-  }
-  return -1;
-}
-
-int main()
-{
-  int t = 0, T, i, res;
-  scanf("%d", &T);
-  while (T > t++)
-  {
-    scanf("%d", &n);
-    for (i = 0; i < n; ++i)
-      scanf("%d", &arr1[i]);
-    for (i = 0; i < n; ++i)
-      scanf("%d", &arr2[i]);
-    res = computed();
-    printf("Case %d: ", t);
-    if (res == -1)
-      printf("Not possible\n");
-    else
-      printf("Possible from station %d\n", res + 1);
   }
   return 0;
 }
