b8c3439076ec2c3a7db95e4698f6ab0eda5f4f84
jzplp <jzplp@qq.com>
2026-08-10 22:06:45 +0800

    ac

--- 1.cpp
+++ 1.cpp
@@ -12,30 +12,32 @@ void computed()
   int wnum, step;
   int i, j, k;
   int num2 = 0;
+  memset(steps, 0, sizeof(steps));
   for (wnum = 0;; ++wnum)
   {
     memset(arrs, 0, sizeof(arrs));
-    memset(steps, 0, sizeof(steps));
     // 计算初始值
     num2 = 0;
-    for (i = (wnum - 2) * s + 1; i <= (wnum - 1) * s; ++i)
+    for (i = (wnum - 1) * s - 1; i < wnum * s - 1; ++i)
     {
-      if (i < 0)
+      if (i < 0 || i >= n)
         continue;
-      if (i >= n)
-        return;
       arrs[arrn[i]] += 1;
       if (arrs[arrn[i]] == 2)
         ++num2;
     }
-    for (step = 1; step <= s; ++step)
+    for (step = 0; step < s; ++step)
     {
       i = step + (wnum - 1) * s;
       if (i - 1 >= n)
         return;
       // 减去前一个，增加下一个
-      if (arrs[arrn[i - 1]] == 2)
-        --num2;
+      if (i - 1 >= 0)
+      {
+        if (arrs[arrn[i - 1]] == 2)
+          --num2;
+        arrs[arrn[i - 1]]--;
+      }
       if (i + s - 1 < n)
       {
         ++arrs[arrn[i + s - 1]];
@@ -59,7 +61,7 @@ int main()
       scanf("%d", &arrn[i]);
     computed();
     j = 0;
-    for (i = 1; i <= s; ++i)
+    for (i = 0; i < s; ++i)
     {
       if (steps[i] == 0)
         ++j;
