7bad2488dbff62bb6551c1fdbc5d13fc6f6bd640
jzplp <jzplp@qq.com>
2026-08-10 16:48:48 +0800

    ac

--- 1.cpp
+++ 1.cpp
@@ -5,14 +5,20 @@
 
 int arrInput[2 * MAXM]; // 所有门的入口
 int n, m;
-int arrm[MAXM];
+int arrm[MAXM];  // 缓存计算过的门值
 int arrnOnei;    // 入口值开始变为1的编号
 int outM;        // 出口门编号
 int zeroV, oneV; // 全0和全1时的值
 
+void clearArrm()
+{
+  for (int i = 1; i <= m; ++i)
+    arrm[i] = -1;
+}
+
 void init()
 {
-  int i, j;
+  int i;
   // 找到出口元素
   memset(arrm, 0, sizeof(arrm));
   for (i = 1; i <= 2 * m; ++i)
@@ -28,38 +34,42 @@ void init()
 
 int getDeepValue(int i)
 {
-  if (i < 0)
+  if (i <= 0)
   {
     if (-i <= arrnOnei)
       return 1;
     return 0;
   }
+  if (arrm[i] >= 0)
+    return arrm[i];
   if (getDeepValue(arrInput[2 * i]) && getDeepValue(arrInput[2 * i - 1]))
+  {
+    arrm[i] = 0;
     return 0;
+  }
+  arrm[i] = 1;
   return 1;
 }
 
 void getInitValue()
 {
-  int i, j, k;
   // 计算入口值全0
   arrnOnei = 0;
+  clearArrm();
   zeroV = getDeepValue(outM);
-  arrnOnei = n+1;
+  arrnOnei = n + 1;
+  clearArrm();
   oneV = getDeepValue(outM);
 }
 
 int computed(int beg, int end)
 {
   int mid = (beg + end) / 2;
-  // printf("--- %d %d %d\n", beg, mid, end);
   if (beg >= mid || beg == end)
     return end;
-  int v;
   arrnOnei = mid;
-  v = getDeepValue(outM);
-  // printf("--- %d\n", v);
-  if (v == zeroV)
+  clearArrm();
+  if (getDeepValue(outM) == zeroV)
     beg = mid;
   else
     end = mid;
@@ -76,9 +86,7 @@ int main()
     for (i = 1; i <= 2 * m; ++i)
       scanf("%d", &arrInput[i]);
     init();
-    // printf("%d\n", outM);
     getInitValue();
-     // printf("%d %d \n", zeroV, oneV);
     if (zeroV == oneV)
     {
       for (i = 1; i <= n; ++i)
@@ -86,8 +94,7 @@ int main()
       putchar('\n');
       continue;
     }
-    j = computed(0, n+1);
-    // printf("%d\n", j);
+    j = computed(0, n + 1);
     for (i = 1; i <= n; ++i)
     {
       if (i < j)
