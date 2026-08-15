1931aa4f32b14d70c881ea747232b67b71f7e326
jzplp <jzplp@qq.com>
2026-08-15 23:28:13 +0800

    进展

--- 1.cpp
+++ 1.cpp
@@ -1,53 +1,49 @@
 #include <stdio.h>
-#include <string.h>
-#define MAXN 100005
+#include <map>
+#define MAXN 200005
+using namespace std;
 
-int arrn[MAXN];
-int s, n;
-int arrs[MAXN];
-int steps[MAXN];
+int arr[MAXN];
+int last[MAXN];
+int n;
+map<int, int> mp;
 
-void computed()
+void init()
 {
-  int wnum, step;
   int i, j, k;
-  int num2 = 0;
-  memset(steps, 0, sizeof(steps));
-  for (wnum = 0;; ++wnum)
+  mp.clear();
+  for (i = 1; i <= n; ++i)
   {
-    memset(arrs, 0, sizeof(arrs));
-    // 计算初始值
-    num2 = 0;
-    for (i = (wnum - 1) * s - 1; i < wnum * s - 1; ++i)
-    {
-      if (i < 0 || i >= n)
-        continue;
-      arrs[arrn[i]] += 1;
-      if (arrs[arrn[i]] == 2)
-        ++num2;
-    }
-    for (step = 0; step < s; ++step)
-    {
-      i = step + (wnum - 1) * s;
-      if (i - 1 >= n)
-        return;
-      // 减去前一个，增加下一个
-      if (i - 1 >= 0)
-      {
-        if (arrs[arrn[i - 1]] == 2)
-          --num2;
-        arrs[arrn[i - 1]]--;
-      }
-      if (i + s - 1 < n)
-      {
-        ++arrs[arrn[i + s - 1]];
-        if (arrs[arrn[i + s - 1]] == 2)
-          ++num2;
-      }
-      if (num2 > 0)
-        steps[step] = 1;
-    }
+    if (!mp[arr[i]])
+      last[i] = 0;
+    else
+      last[i] = mp[arr[i]];
+    mp[arr[i]] = i;
+    // printf("%d ", last[i]);
   }
+  // putchar('\n');
+}
+
+bool computed(int beg, int end)
+{
+  if (beg >= end)
+    return true;
+  int i;
+  mp.clear();
+  for (i = end; i >= beg; --i)
+  {
+    // printf("%d %d %d\n", i, arr[i], mp[arr[i]]);
+    if (mp[arr[i]])
+      continue;
+    // printf("%d %d %d\n", i, arr[i], mp[arr[i]]);
+    mp[arr[i]] = 1;
+    if (last[i] < beg)
+      break;
+  }
+  // printf("-- %d %d %d\n", beg, end, i);
+  if (i < beg)
+    return false;
+  return computed(beg, i - 1) && computed(i + 1, end);
 }
 
 int main()
@@ -56,17 +52,14 @@ int main()
   scanf("%d", &t);
   while (t--)
   {
-    scanf("%d %d", &s, &n);
-    for (i = 0; i < n; ++i)
-      scanf("%d", &arrn[i]);
-    computed();
-    j = 0;
-    for (i = 0; i < s; ++i)
-    {
-      if (steps[i] == 0)
-        ++j;
-    }
-    printf("%d\n", j);
+    scanf("%d", &n);
+    for (i = 1; i <= n; ++i)
+      scanf("%d", &arr[i]);
+    init();
+    if (computed(1, n))
+      printf("non-boring\n");
+    else
+      printf("boring\n");
   }
   return 0;
 }
